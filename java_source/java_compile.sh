472936632f0a1bf179aed8e8f06c3b84411a8d6b
namke <john@namke.com>
2021-02-22 09:35:01 +0000

    Relocated the hex2wav executable into a tools directory; rebuilt.

--- java_source/java_compile.sh
+++ java_source/java_compile.sh
@@ -2,4 +2,5 @@ rm -fr */*.class
 javac -classpath . */*.java
 jar cvfm hex2wav.jar MANIFEST.MF */*.class
 #java -cp . controllPanel/Main_WavBootLoader
-java -jar hex2wav.jar /Users/xcorex/Documents/Projects/AVR/8BitMixtapeNeo/TinyAudioBoot/build/AudioBootAttiny85_AudioPB3_LEDPB1.hex
+#java -jar hex2wav.jar /Users/xcorex/Documents/Projects/AVR/8BitMixtapeNeo/TinyAudioBoot/build/AudioBootAttiny85_AudioPB3_LEDPB1.hex
+mv hex2wav.jar ../tools/hex2wav
