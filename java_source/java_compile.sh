226b494ec5ddb2fe5b6ebc899357982cfde44005
namke <john@namke.com>
2021-02-24 20:42:50 +0000

    First cut Makefile.

--- java_source/java_compile.sh
+++
@@ -1,6 +0,0 @@
-rm -fr */*.class
-javac --release 8 -classpath . */*.java
-jar cvfm hex2wav.jar MANIFEST.MF */*.class
-#java -cp . controllPanel/Main_WavBootLoader
-#java -jar hex2wav.jar /Users/xcorex/Documents/Projects/AVR/8BitMixtapeNeo/TinyAudioBoot/build/AudioBootAttiny85_AudioPB3_LEDPB1.hex
-mv hex2wav.jar ../tools/hex2wav
